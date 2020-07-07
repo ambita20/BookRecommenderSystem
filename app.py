f722e3de2a0dfde0199354a82cc9e7a1a324c7ea
Ambita Surlekar <ambitasurlekar@gmail.com>
2020-07-06 22:43:13 -0700

    Model changes and added flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import pyttsx3
+app = Flask(__name__)
+
+@app.route("/api/v1.0/recommendations/<int:id>", methods=["GET"])
+def get_recomendations(id):
+    print("product_id: " + str(id))
+    return engine.get_recommendations(id)
+
+if __name__ == "__main__":
+    app.run()
